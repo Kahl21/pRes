13e194549145b5bf05e46b230e33f28eda6de84f
kahl21 <kahlpartlow@gmail.com>
2017-11-14 16:50:24 -0800

    stuff got done i think

--- Assets/Scripts/UI/LevelLoader.cs
+++ Assets/Scripts/UI/LevelLoader.cs
@@ -8,6 +8,8 @@ public class LevelLoader : MonoBehaviour
 {
     public GameObject Loadingscreen;
     public GameObject loadingobject;
+    float spin;
+    private bool loading = true;
 
     public void LoadLevel(int sceneIndex)
     {
@@ -16,19 +18,38 @@ public class LevelLoader : MonoBehaviour
 
     IEnumerator LoadAsyncrounously(int sceneIndex)
     {
-        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Single);
 
         Loadingscreen.SetActive(true);
 
+        loading = true;
+
         while(!operation.isDone)
-        {
-            float progress = 10f;
+       {
+            //float progress = 10f;
+            //spin = loadingobject.GetComponent<RectTransform>().rotation.z;
 
             //loadingBar.value = progress;
             //PercentLoaded.text = (progress * 100f) + "%";
-            loadingobject.transform.Rotate(Vector3.up, progress * Time.deltaTime);
+            //loadingobject.transform.Rotate(Vector3.up, progress * Time.deltaTime);
+            //loadingobject.GetComponent<RectTransform>().Rotate(new Vector3(0,0,spin+10));
 
             yield return null;
-        }
+       }
+        
     }
+
+    public void Update()
+    {
+        Debug.Log(loading);
+
+        if (loading)
+        {
+            spin = loadingobject.GetComponent<RectTransform>().rotation.z;
+            loadingobject.GetComponent<RectTransform>().Rotate(new Vector3(0,0,spin+10));
+            loading = true;
+        }
+
+        }
+
 }
---
+++ Assets/Scripts/UI/Spin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+    float spin;
+
+    // Use this for initialization
+    void Start () {
+        spin = gameObject.GetComponent<RectTransform>().rotation.z;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.GetComponent<RectTransform>().Rotate(new Vector3(0, 0, spin + 10));
+    }
+}
